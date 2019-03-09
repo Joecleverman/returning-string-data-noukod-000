76679d3b341109b59e2a3fd64b094c5e9ddee04e
Josue Abraham <josuea@noukod.com>
2019-03-09 07:12:29 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'home', to: 'static#home'
-  resources :orders
-  resources :invoices
-  resources :products
-  get '/products/:id/inventory', to: 'products#inventory'
-  get '/products/:id/description', to: 'products#description'
+  resources :posts, only: [:index, :show, :new, :create, :edit]
+  get '/posts/:id/body', to: 'posts#body'
 end
